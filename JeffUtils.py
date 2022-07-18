e444b4b0ff8cb63424a79094175039058f1102b5
Jeff <31432625+Crumbletheundead@users.noreply.github.com>
2022-07-18 17:38:02 +0800

    fixed column clustering

--- JeffUtils.py
+++ JeffUtils.py
@@ -1,14 +1,16 @@
+#Docstrings are designed to integrate with Sphinx package
+
 import pandas as pd 
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 import matplotlib.axes
 
-def get_second_deriv(series:pd.Series)->pd.Series:
+def get_second_deriv(continuous_data:pd.Series)->pd.Series:
     """calculates the second derivative 
     at every point in the series
     """
-    df = series.rename('x').to_frame()
+    df = continuous_data.rename('x').to_frame()
     df['next'] = df['x'].shift()
     df['prev'] = df['x'].shift(-1)
     return df.apply(
@@ -23,20 +25,20 @@ def graph_elbow(series:pd.Series)->int:
     then returns the optimum number of clusters
     """
     ax = sns.lineplot(x=series.index,y=series)
-    elbow = get_second_deriv(series).argmax()
+    elbow = get_second_deriv(series).argmax()+series.index.min()
     ax.axvline(elbow,0,ax.get_ylim()[1],color='red',**{'alpha':0.5})
     ax.set_xlabel('Parameter')
     return elbow
 
-def graph_cluster_wc(df: pd.DataFrame, tokenized_col: str,
-                     cluster_col: str, clusterId: int, head:int=5,
+def graph_cluster_wc(df:pd.DataFrame, tokenized_col:str,
+                     cluster_col:str, clusterId:int, head:int=5,
                     ax:matplotlib.axes=None):
     """graphs the word count of each unique word in a cluster of documents
-    each document must be tokenized
+    each document must be tokenized.
     
     Keyword arguments:
-    df -- a dataframe with a column contianing tokenized documents, 
-        and a column of ints containing the cluster the document belongs to
+    df -- a dataframe with a column contianing tokenized documents, and 
+        a column of ints containing the cluster the document belongs to
     tokenized_col -- name of column containing tokenized document
     cluster_col -- name of column contianing cluster flags
     clusterId -- integer indicating which cluster you want to graph
@@ -47,6 +49,12 @@ def graph_cluster_wc(df: pd.DataFrame, tokenized_col: str,
     tokens_exploded = (
         cluster[tokenized_col]
         .explode()
+        #turn to dataframe for dropping duplicates
+        .to_frame().reset_index() 
+        #drop duplicates to ensure column descriptions 
+        #with repeating words don't mess with calculation
+        #of percentage of descriptoins the word appears in
+        .drop_duplicates()[tokenized_col]
         .value_counts()
         .head(head)
     )
@@ -62,17 +70,24 @@ def graph_cluster_wc(df: pd.DataFrame, tokenized_col: str,
     else:
         barAx.set_xlabel(xTitle)
 
-def graph_all_cluster_wc(col_desc:pd.DataFrame,cluster_col:str):
+def graph_all_cluster_wc(col_desc:pd.DataFrame,
+                         cluster_col:str,figsize=(15,8)):
     """graphs top 5 most common words in each cluster
+    ensures that subplot arrangement are as square as possible
+    instead of one long list
     """
     classes = np.sort(col_desc[cluster_col].unique())
     r = int(np.ceil(np.sqrt(classes.shape[0])))
     c = int(np.ceil(classes.shape[0]/r))
-    fig, ax = plt.subplots(r,c,figsize=(15,8))
+    fig, ax = plt.subplots(r,c,figsize=figsize)
     for i in range(r):
         for j in range(c):
             if i*c+j<classes.shape[0]:
                 cluster = classes[i*c+j]
-                graph_cluster_wc(col_desc,'tokenized',cluster_col,cluster,ax=ax[i][j])
+                graph_cluster_wc(
+                    col_desc, 'tokenized',
+                    cluster_col, cluster,
+                    ax=ax[i][j]
+                )
                 ax[i][j].title.set_text(f'cluster {cluster}')
     plt.tight_layout()
