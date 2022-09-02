6f7dc25782b15d47948ab775095bb250e3047395
Jeff Chen Fan Wang <j3ff123y@gmail.com>
2022-09-02 19:22:39 -0400

    str churn eda

--- clean_raw.py
+++ clean_raw.py
@@ -12,7 +12,7 @@ def clean_raw():
 	try:
 		raw_data = pd.read_csv(DATASET_FOLDER_PATH+IMPORT_FILE_NAME)
 	except:
-		print(f'raw data {IMPORT_FILE_NAME} cannot be found IN {DATASET_FOLDER_PATH}')
+		print(f'raw data {IMPORT_FILE_NAME} cannot be found in {DATASET_FOLDER_PATH}')
 		return
 	raw_data = raw_data.set_index('Customer_ID')
 	raw_data.columns = map(str.lower,raw_data.columns)
