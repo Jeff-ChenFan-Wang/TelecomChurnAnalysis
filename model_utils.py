d5da49551f0286199113fb6f45ec93a1cd3e9c7f
j3ff123y <j3ff123y@gmail.com>
2022-09-26 00:55:01 -0500

    finished pipeline factory and starting model building

---
+++ model_utils.py
@@ -0,0 +1,158 @@
+import pandas as pd
+import numpy as np
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import (
+    StandardScaler, 
+    OneHotEncoder, 
+    FunctionTransformer
+)
+from sklearn.compose import ColumnTransformer
+from sklearn.impute import SimpleImputer
+
+class customPipe(Pipeline):
+    """custom subclass of the sklearn Pipeline that has a working
+    get_feature_names_out() method since the official one is still broken as of 
+    version 1.1.2
+    """
+    def get_feature_names_out(self, input_features=None):
+        return self.steps[-1][1].get_feature_names_out()
+    
+class PipelineFactory:
+    """A factory class for producing different sklearn pipelines
+    that also retains feature names. As of version 1.1.2 for sci-kit learn,
+    pipelines still cannot retain feature names after transformation, hence
+    the customized pipelines produced by this class add a function transformer 
+    to retain feature names.
+    """
+    
+    def __init__(self, raw_data:pd.DataFrame):
+        """initilizes the factory by determining which columns are categorical
+        and which are numerical so output pipeline can make appropriate 
+        transformations
+
+        Args:
+            raw_data (pd.DataFrame): raw data for pipelines to be fitted against
+        """
+        self.original_cols = raw_data.columns
+        self.cat_cols = raw_data.select_dtypes('object').columns
+        
+        #since certain binary columns are encoded as integers and floats, 
+        #we have to separate them out from the data to determine which are
+        #truly numerical columns and which are actually binary columns
+        num_cols = raw_data.select_dtypes(np.number).columns
+        self.bin_cols = num_cols[raw_data[num_cols].nunique()==2]
+        num_cols = pd.Index(set(num_cols)-set(self.bin_cols))
+        self.num_cols = num_cols
+    
+    def create_pipe(self, *, impute:bool, normalize:bool)->customPipe:
+        """Factory method to create pipelines. Define whether the pipeline
+        needs to impute missing data, and whether numerical columns should 
+        be normalized (in the sense that it should be standardized). 
+        
+        The method will automatically determine which columns are categorical 
+        and which are numerical based on the columns datatype for appropriate 
+        transformations. Binary columns will not be normalized nor one-hot 
+        encoded in any of the pipelines. All pipelines created will one-hot
+        encode categorical columns. 
+
+        Args:
+            impute (bool): whether to use mode impution on missing data
+            normalize (bool): whether to standardize data
+
+        Raises:
+            Exception: impute or normalize isn't defined
+            NotImplementedError: combination of impute or normalize hasn't been
+                implemented.
+
+        Returns:
+            Pipeline: sklearn pipeline with necessary transformers according to 
+                input parameters, and also a functioning feature names method.
+        """
+        if (impute is None) or (normalize is None):
+            raise Exception('please instruct whether to impute or scale')
+        if impute & normalize:
+            return self.make_impute_ohe_scale_pipe(self.cat_cols,self.num_cols)
+        elif impute & ~normalize:
+            return self.make_impute_ohe_pipe(self.cat_cols,self.num_cols)
+        elif ~impute & ~normalize:
+            return self.make_ohe_pipe(self.cat_cols,self.num_cols)
+        else:
+            raise NotImplementedError('Cannot determine pipeline type')
+    
+    def make_impute_ohe_scale_pipe(self)->customPipe:
+        """creates a pipeline that imputes data using mode impuation, then
+        normalizes and one-hot encodes appropriate columns
+
+        Returns:
+            customPipe: basically a sklearn pipeline but with a functioning
+                get_feature_names_out() method.
+        """
+        
+        pipe = customPipe([
+            ('impute',SimpleImputer(strategy='most_frequent')),
+            ( #A function transformer is needed to turn the previous step output
+             #into a pandas dataframe with named columns so that feature
+             #names can be maintained thoughout the pipeline
+                'retainFeatureName',
+                FunctionTransformer(
+                    lambda x: pd.DataFrame(x, columns=self.original_cols)
+                )
+            ),
+            (
+                'columnTransform',
+                ColumnTransformer([
+                    ('categorical_vars',OneHotEncoder(),self.cat_cols),
+                    ('numeric_vars',StandardScaler(),self.num_cols),
+                    ('binary_vars','passthrough',self.bin_cols)
+                ])
+            )
+        ])
+        return pipe
+    
+    def make_impute_ohe_pipe(self)->customPipe:
+        """creates a pipeline that imputes data using mode impuation, then 
+        one-hot encodes appropriate columns but does not normalize numerical
+        columns
+
+        Returns:
+            customPipe: basically a sklearn pipeline but with a functioning
+                get_feature_names_out() method.
+        """
+        pipe = customPipe([
+            ('impute',SimpleImputer(strategy='most_frequent')),
+            (
+                'retainFeatureName',
+                FunctionTransformer(
+                    lambda x: pd.DataFrame(x, columns=self.original_cols)
+                )
+            ),
+            (
+                'columnTransform',
+                ColumnTransformer([
+                    ('categorical_vars',OneHotEncoder(),self.cat_cols),
+                    ('numeric_vars','passthrough',self.num_cols),
+                    ('binary_vars','passthrough',self.bin_cols)
+                ])
+            )
+        ])
+        return pipe
+    
+    def make_ohe_pipe(self)->customPipe:
+        """creates a pipeline that one-hot encodes appropriate columns but 
+        does not normalize numerical columns nor impute any missing data.
+
+        Returns:
+            customPipe: basically a sklearn pipeline but with a functioning
+                get_feature_names_out() method.
+        """
+        pipe = customPipe([
+             (
+                'columnTransform',
+                ColumnTransformer([
+                    ('categorical_vars',OneHotEncoder(),self.cat_cols),
+                    ('numeric_vars','passthrough',self.num_cols),
+                    ('binary_vars','passthrough',self.bin_cols)
+                ])
+            )
+        ])
+        return pipe
