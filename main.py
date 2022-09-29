e7534421de46aa09579c2dd7c825fc49118e97a7
j3ff123y <j3ff123y@gmail.com>
2022-09-29 06:43:21 -0500

    mvp dashboard mockup

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from pathlib import Path
 
 app = FastAPI(title="TelecomChurn")
 
+BASE_PATH = Path(__file__).resolve().parent
+TEMPLATES = Jinja2Templates(directory=str(BASE_PATH / "dashboard"))
+
+
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
+async def root(request: Request) -> dict:
+    return TEMPLATES.TemplateResponse(
+        "index.html",
+        {"request": request},
+    )
